d984e5d2048ed11a593712b1e6d8aa5c1e4be2b9
Noha Kareem <NohaKareem@users.noreply.github.com>
2020-11-19 21:55:50 -0500

    Update video style + add interactive scrubber

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@
 	const PLAY_BTN = document.querySelector('.play');
 	const SPEED_BTN = document.querySelector('.speed');
 	const STOP_BTN = document.querySelector('.stop');
-	const SCRUBBER = document.querySelector('.scrubberCon div');
+	const SCRUBBER_CON = document.querySelector('.scrubberCon');
+	const SCRUBBER = document.querySelector('.scrubberCon div');//~
+	const SCRUB_SLIDER = document.querySelector('.scrubSlider');
 	const TIME_LAPSED = document.querySelector('.scrubberCon span');
 	const PLAY_FA = `<i class="fa fa-play" aria-hidden="true"></i>`;
 	const PAUSE_FA = `<i class="fa fa-pause" aria-hidden="true"></i>`;
@@ -19,11 +21,15 @@
 	// pad input with leading zero
 	let padZeros = num => { return `${num}`.padStart(2, '0'); }
 
-	let updateScrubber = _ => {
-		let minutes = Math.floor(VID.currentTime / 60);
-		let seconds = Math.floor(VID.currentTime - minutes * 60);
-		TIME_LAPSED.innerHTML = `${padZeros(minutes)}:${padZeros(seconds)}`;
-	}
+	// let updateScrubber = _ => {
+	// 	let minutes = Math.floor(VID.currentTime / 60);
+	// 	let seconds = Math.floor(VID.currentTime - minutes * 60);
+	// 	TIME_LAPSED.innerHTML = `${padZeros(minutes)}:${padZeros(seconds)}`;
+
+	// 	// let scrubberConLen = SCRUBBER_CON * (VID.currentTime / VID.duration); //~~
+	// 	// SCRUBBER.style.width = `${scrubberConLen}px`;
+	// 	SCRUB_SLIDER.value = e.currentTarget.max * VID.currentTime / VID.duration;
+	// }
 
 	// toggle play pause 
 	PLAY_BTN.addEventListener('click', _ => {
@@ -55,7 +61,18 @@
 	STOP_BTN.addEventListener('click', stopVid);
 	VID.addEventListener('ended', stopVid);
 
-	// update scrubber
-	VID.addEventListener('timeupdate', updateScrubber);
+	// update scrubber ~
+	VID.addEventListener('timeupdate', e => {
+		let minutes = Math.floor(VID.currentTime / 60);
+		let seconds = Math.floor(VID.currentTime - minutes * 60);
+		TIME_LAPSED.innerHTML = `${padZeros(minutes)}:${padZeros(seconds)}`;
+
+		SCRUB_SLIDER.value = e.currentTarget.max * VID.currentTime / VID.duration;
+	});
+
+	// update scrub slider on input 
+	SCRUB_SLIDER.addEventListener('input', e => {
+		VID.currentTime = e.currentTarget.value * VID.duration / e.currentTarget.max;
+	});
 })();
 
